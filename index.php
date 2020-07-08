0c11fa2a09860b432e0e2178898dba8392df3b79
andreytereshenko <andre.tereshenko@gmail.com>
2020-07-08 18:38:44 +0300

    init

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>NIX Education</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<header>
+    <nav class="topnav">
+        <a href="#">Урок 1</a>
+        <a href="#">Урок 2</a>
+    </nav>
+</header>
+
+<main class="main">
+        <h1>Hello, NIX Education</h1>
+</main>
+
+<footer class="footer">
+
+        <ul id="icon">
+            <li>
+                <a href="https://github.com/andreytereshenko">
+                    <img src="img/git.png" alt="github" width="40" height="40">
+                </a>
+            </li>
+        </ul>
+
+</footer>
+
+</body>
+
+</html>
